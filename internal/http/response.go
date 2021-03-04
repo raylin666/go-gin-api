9e6cf31869389132bb5342454e624cc1bb2fcfc1
linshan <1099013371@qq.com>
2021-03-04 23:50:02 +0800

    feat

--- internal/http/response.go
+++ internal/http/response.go
@@ -10,10 +10,8 @@ import (
 )
 
 var (
-	output *Output
-	isSetOutput = false
-	responseFormat = DefaultResponseFormat
-	format = []string{FormatJSON, FormatXML, FormatYAML, FormatJSONP}
+	// 获取返回格式类型集合
+	Format = []string{FormatJSON, FormatXML, FormatYAML, FormatJSONP}
 )
 
 const (
@@ -21,25 +19,19 @@ const (
 	FormatXML   = "XML"
 	FormatYAML  = "YAML"
 	FormatJSONP = "JSONP"
-
-	DefaultResponseFormat = FormatJSON
 )
-
-// 获取返回格式类型
-func GetFormat() []string {
-	return format
-}
-
-type Output struct {
-	Builder *Builder
-
-	// 输出类型, 例如：JSON、XML、YAML
-	Format string
-}
 
 type H map[string]interface{}
 
-// 构建输出数据结构体
+// 输出结构体
+type Output struct {
+	Builder Builder
+
+	// 输出格式, 如 JSON XML JSONP
+	Format  string
+}
+
+// 构建响应结构体
 type Builder struct {
 	// HTTP 状态码
 	HttpCode int
@@ -62,110 +54,76 @@ type Builder struct {
 			},
 		},
 	})
- */
-func Response(ctx *gin.Context, o *Output) {
-	httpCode, out := handlerOutputResponse(o)
+*/
+func Response(ctx *gin.Context, out *Output)  {
+	code, output := handlerResponse(out)
 
-	out.Builder.ResponseTime = time.Now().Sub(http.GetRequest().StartTime)
+	output.Builder.ResponseTime = time.Now().Sub(http.GetRequest().StartTime)
 
-	switch responseFormat {
+	switch output.Format {
 	case FormatJSON:
-		builderResponseJSON(ctx, httpCode, out)
+		builderResponseJSON(ctx, code, output)
 	case FormatJSONP:
-		builderResponseJSONP(ctx, httpCode, out)
+		builderResponseJSONP(ctx, code, output)
 	case FormatXML:
-		builderResponseXML(ctx, httpCode, out)
+		builderResponseXML(ctx, code, output)
 	case FormatYAML:
-		builderResponseYAML(ctx, httpCode, out)
+		builderResponseYAML(ctx, code, output)
 	default:
-		builderResponseJSON(ctx, httpCode, out)
+		builderResponseJSON(ctx, code, output)
 	}
 }
 
-func handlerOutputResponse(out *Output) (httpCode int, o *Output) {
-	if out.Format != "" {
-		responseFormat = out.Format
+func (output Output) Success(ctx *gin.Context, h H) {
+	output.Builder.Data = h
+	Response(ctx, &output)
+}
+
+func SuccessResponse(ctx *gin.Context, h H) {
+	output := &Output{
+		Builder: Builder{
+			Data: h,
+		},
 	}
 
-	if out.Builder.Code == 0 {
-		out.Builder.Code = constant.StatusOK
+	Response(ctx, output)
+}
+
+func (output Output) Error(ctx *gin.Context, code int) {
+	output.Builder.Code = code
+	Response(ctx, &output)
+}
+
+func ErrorResponse(ctx *gin.Context, code int) {
+	output := &Output{
+		Builder: Builder{
+			Code: code,
+		},
 	}
 
-	httpCode = out.Builder.Code
-	if httpCode > 600 {
-		if out.Builder.HttpCode == 0 {
-			httpCode = constant.StatusOK
+	Response(ctx, output)
+}
+
+func handlerResponse(output *Output) (code int, out *Output) {
+	if output.Builder.Code == 0 {
+		output.Builder.Code = constant.StatusOK
+	}
+
+	// code HTTP 状态码
+	code = output.Builder.Code
+	if code > 600 {
+		if output.Builder.HttpCode == 0 {
+			code = constant.StatusOK
 		} else {
-			httpCode = out.Builder.HttpCode
+			code = output.Builder.HttpCode
 		}
 	}
 
-	if out.Builder.Message == "" {
-		out.Builder.Message = constant.GetStatusText(out.Builder.Code)
+	if output.Builder.Message == "" {
+		output.Builder.Message = constant.GetStatusText(output.Builder.Code)
 	}
 
-	return httpCode, out
-}
-
-func SetOutputResponse(out *Output) {
-	_, output = handlerOutputResponse(out)
-	isSetOutput = true
-}
-
-func GetOutputResponse() *Output {
-	return output
-}
-
-func IsSetOutputResponse() bool {
-	return isSetOutput
-}
-
-/**
-	http.SetOutputResponse(http.Output{Builder: http.Builder{Code: 200}, Format: http.FormatJSON})
-	http.SuccessResponse(ctx, http.H{
-		"message": "hello gin-api.",
-	})
- */
-func SuccessResponse(ctx *gin.Context, h H)  {
-	var out *Output
-	if IsSetOutputResponse() {
-		out = &Output{
-			Builder: &Builder{
-				Code: output.Builder.Code,
-				Data: h,
-			},
-			Format: output.Format,
-		}
-	} else {
-		out = &Output{
-			Builder: &Builder{
-				Data: h,
-			},
-		}
-	}
-
-	Response(ctx, out)
-}
-
-func ErrorResponse(ctx *gin.Context, code int)  {
-	var out *Output
-	if IsSetOutputResponse() {
-		out = &Output{
-			Builder: &Builder{
-				Code: code,
-				Data: output.Builder.Data,
-			},
-			Format: output.Format,
-		}
-	} else {
-		out = &Output{
-			Builder: &Builder{
-				Code: code,
-			},
-		}
-	}
-
-	Response(ctx, out)
+	return code, output
 }
 
 // MarshalXML allows type H to be used with xml.Marshal.
@@ -190,32 +148,32 @@ func (h H) MarshalXML(e *xml.Encoder, start xml.StartElement) error {
 	return e.EncodeToken(xml.EndElement{Name: start.Name})
 }
 
-func builderResponseJSON(ctx *gin.Context, httpCode int, out *Output) {
-	ctx.JSON(httpCode, builderResponse(
+func builderResponseJSON(ctx *gin.Context, code int, out *Output) {
+	ctx.JSON(code, builderResponse(
 		out.Builder.Code,
 		out.Builder.Message,
 		out.Builder.Data,
 		out.Builder.ResponseTime))
 }
 
-func builderResponseXML(ctx *gin.Context, httpCode int, out *Output) {
-	ctx.XML(httpCode, builderResponse(
+func builderResponseXML(ctx *gin.Context, code int, out *Output) {
+	ctx.XML(code, builderResponse(
 		out.Builder.Code,
 		out.Builder.Message,
 		out.Builder.Data,
 		out.Builder.ResponseTime))
 }
 
-func builderResponseYAML(ctx *gin.Context, httpCode int, out *Output) {
-	ctx.YAML(httpCode, builderResponse(
+func builderResponseYAML(ctx *gin.Context, code int, out *Output) {
+	ctx.YAML(code, builderResponse(
 		out.Builder.Code,
 		out.Builder.Message,
 		out.Builder.Data,
 		out.Builder.ResponseTime))
 }
 
-func builderResponseJSONP(ctx *gin.Context, httpCode int, out *Output) {
-	ctx.JSONP(httpCode, builderResponse(
+func builderResponseJSONP(ctx *gin.Context, code int, out *Output) {
+	ctx.JSONP(code, builderResponse(
 		out.Builder.Code,
 		out.Builder.Message,
 		out.Builder.Data,
@@ -231,3 +189,5 @@ func builderResponse(code int, message string, data H, responseTime time.Duratio
 		"responseTime": fmt.Sprintf("%s", responseTime),
 	}
 }
+
+
