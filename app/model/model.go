a77301c4a1c5c406744cec3c56247e56a89c6109
linshan <1099013371@qq.com>
2021-03-06 16:07:56 +0800

    model

--- app/model/model.go
+++ app/model/model.go
@@ -1,7 +1,8 @@
 package model
 
 type Model struct {
-	ID        int `gorm:"primary_key" json:"id"`
-	CreatedAt int `json:"created_at"`
-	UpdatedAt int `json:"updated_at"`
+	ID        uint `gorm:"primary_key" json:"id"`
+	CreatedAt int  `json:"created_at"`
+	UpdatedAt int  `json:"updated_at"`
+	DeletedAt int  `gorm:"index"`
 }
