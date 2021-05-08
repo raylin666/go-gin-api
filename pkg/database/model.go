6072bb3c47117588af6d79c5ea96c2ec212c8949
linshan <1099013371@qq.com>
2021-05-08 17:47:58 +0800

    功能迭代

--- pkg/database/model.go
+++ pkg/database/model.go
@@ -4,6 +4,10 @@ import "time"
 
 type Model struct {
 	ID        uint64     `gorm:"primary_key" json:"id"`
+	*ModTime
+}
+
+type ModTime struct {
 	CreatedAt time.Time  `json:"created_at"`
 	UpdatedAt time.Time  `json:"updated_at"`
 	DeletedAt *time.Time `gorm:"index" json:"deleted_at"`
