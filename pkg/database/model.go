362a0699bceb4668fba46fad6da973f904ca93fc
linshan <1099013371@qq.com>
2021-05-08 17:47:23 +0800

    功能迭代

---
+++ pkg/database/model.go
@@ -0,0 +1,10 @@
+package database
+
+import "time"
+
+type Model struct {
+	ID        uint64     `gorm:"primary_key" json:"id"`
+	CreatedAt time.Time  `json:"created_at"`
+	UpdatedAt time.Time  `json:"updated_at"`
+	DeletedAt *time.Time `gorm:"index" json:"deleted_at"`
+}
