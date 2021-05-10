fc50bb10506e5793011eb18fd73f7ab14e4e54b6
linshan <1099013371@qq.com>
2021-05-10 21:33:46 +0800

    model update

--- pkg/database/model.go
+++ pkg/database/model.go
@@ -1,14 +1,6 @@
 package database
 
-import "time"
-
 type Model struct {
-	ID        uint64     `gorm:"primary_key" json:"id"`
-	*ModTime
-}
-
-type ModTime struct {
-	CreatedAt time.Time  `json:"created_at"`
-	UpdatedAt time.Time  `json:"updated_at"`
-	DeletedAt *time.Time `gorm:"index" json:"deleted_at"`
+	ID        uint64    `gorm:"primary_key" json:"id"`
+	CreatedAt uint64	`json:"created_at"`
 }
